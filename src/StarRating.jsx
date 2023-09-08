9fe89ab0d5c25a3459292a77d60893c28d1210f2
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-09-08 19:40:31 +0330

    Update StarRating component: to support a variable number of stars.

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,3 +1,28 @@
-export default function StarRating() {
-  return <div> Helooo</div>;
+const containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: '16px',
+};
+
+const starContainerStyle = {
+  display: 'flex',
+  gap: '4px',
+};
+
+const textStyle = {
+  lineHeight: '1',
+  margin: '0',
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>👑</span>
+        ))}
+      </div>
+      <p style={textStyle}>10</p>
+    </div>
+  );
 }
